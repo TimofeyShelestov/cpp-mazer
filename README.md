0f586c66b159d35fda6cca5b35c8658b4f73024c
timofei.shelestov <tim.shelestov@gmail.com>
2025-05-14 14:44:17 +0200

    now it is a correct maze, changed dfs implementation, added animation during creation of maze, small fixes

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-small maze generator written in c++
+# small maze generator written in c++
 
-compile mazer.cpp and maze-tools.cpp (Makefile is only for macos) and run ./mazer -r {number of rows} -c {number of cols}
-or run ./mazer --help
+### uses dfs to make maze from a grid with defined width and length. happens in real time
+
+copy the code from github and run make (only for macos)
+or compile mazer and maze_tool using your's c++ compiler accordingly to your os
+
+to run maze (in terminal mode) run **./mazer -r {number of rows} -c {number of columns}** or **--help** for help
+
+graphical mode soon... (with **raylib**)
--- maze_tools.cpp
+++ maze_tools.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <random>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
@@ -10,20 +12,20 @@ struct node
   bool bottom = true;
   bool left = true;
   bool right = true;
-  bool visited;
+  bool visited = false;
 };
 
 void grid_coords_setup(vector<vector<node>>& grid, int rows, int cols) {
   for (int row = 0; row < rows; ++row) {
     for (int col = 0; col < cols; ++col) {
       grid[row][col].x = row;
-      grid[row][col].y = cols;
+      grid[row][col].y = col;
     }
   }
 }
 
 vector<pair<int, int>> get_unvisited_neighbours(vector<vector<node>>& grid, int x, int y, int rows, int cols) {
-  vector<pair<int, int>> neigbours;
+  vector<pair<int, int>> neighbours;
   const int dx[] = {-1, 0, 1, 0};
   const int dy[] = {0, 1, 0, -1};
   
@@ -33,53 +35,12 @@ vector<pair<int, int>> get_unvisited_neighbours(vector<vector<node>>& grid, int 
   
     if (nx >= 0 && nx < rows && ny >= 0 && ny < cols) {
       if (!grid[nx][ny].visited) {
-        neigbours.push_back({nx, ny});
+        neighbours.push_back({nx, ny});
       }
     }
   }
-  return neigbours;
-}
-
-void remove_walls(vector<vector<node>>& grid, int curr_x, int curr_y, int neighbour_x, int neighbour_y ) {
-  if (curr_x == neighbour_x && curr_y < neighbour_y) {
-    grid[curr_x][curr_y].top = false;
-    grid[neighbour_x][neighbour_y].bottom = false;
-  }
-
-  if (curr_y == neighbour_y && curr_x < neighbour_x) {
-    grid[curr_x][curr_y].right = false;
-    grid[neighbour_x][neighbour_y].left = false;
-  }
-
-  if (curr_x == neighbour_x && curr_y > neighbour_y) {
-    grid[curr_x][curr_y].bottom = false;
-    grid[neighbour_x][neighbour_y].top = false;
-  }
-
-  if (curr_y == neighbour_y && curr_x > neighbour_x) {
-    grid[curr_x][curr_y].left = false;
-    grid[neighbour_x][neighbour_y].right = false;
-  }
-}
-
-void dfs(vector<vector<node>>& grid, int curr_x, int curr_y,  int rows, int cols) {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  grid[curr_x][curr_y].visited = true;
   
-  while (true) {
-    vector<pair<int, int>> neighbours = get_unvisited_neighbours(grid, curr_x, curr_y, rows, cols);
-    if (neighbours.empty()) {
-      break;
-    }
-    std::uniform_int_distribution<> dist(0, neighbours.size() - 1);
-    auto [nx, ny] = neighbours[dist(gen)];
-
-    remove_walls(grid, curr_x, curr_y, nx, ny);
-
-    
-    dfs(grid, nx, ny, rows, cols);
-  }
+  return neighbours;
 }
 
 void print_maze(vector<vector<node>>& grid, int rows, int cols) {
@@ -93,7 +54,7 @@ void print_maze(vector<vector<node>>& grid, int rows, int cols) {
       }
     }
     cout << "+";
-    cout << "\n";
+    cout << endl;
     for (int col = 0; col < cols; ++col) {
       node curr_node = grid[row][col];
       if (curr_node.left == true) {
@@ -103,12 +64,14 @@ void print_maze(vector<vector<node>>& grid, int rows, int cols) {
       }
       cout << "   ";
     }
-    cout << "|";
-    cout << "\n";
+    if (grid[row][cols-1].right == true) {
+      cout << "|";
+    }
+    cout << endl;
   }
   for (int col = 0; col < cols; ++col) {
     node curr_node = grid[rows-1][col];
-    if (curr_node.top == true) {
+    if (curr_node.bottom == true) {
       cout << "+---";
     } else {
       cout << "+   ";
@@ -117,10 +80,49 @@ void print_maze(vector<vector<node>>& grid, int rows, int cols) {
   cout << "+" << endl;
 }
 
+void remove_walls(vector<vector<node>>& grid, int curr_x, int curr_y, int neighbour_x, int neighbour_y ) {
+  if (curr_y > neighbour_y) {
+      grid[curr_x][curr_y].left = false;
+      grid[neighbour_x][neighbour_y].right = false;
+  }
+
+  if (curr_y < neighbour_y) {
+      grid[curr_x][curr_y].right = false;
+      grid[neighbour_x][neighbour_y].left = false;
+  }
+
+  if (curr_x > neighbour_x) {
+      grid[curr_x][curr_y].top = false;
+      grid[neighbour_x][neighbour_y].bottom = false;
+  }
+
+  if (curr_x < neighbour_x) {
+      grid[curr_x][curr_y].bottom = false;
+      grid[neighbour_x][neighbour_y].top = false;
+  }
+}
+
+void dfs(vector<vector<node>>& grid, int curr_x, int curr_y,  int rows, int cols, mt19937& gen) {
+  grid[curr_x][curr_y].visited = true;
+  
+  vector<pair<int, int>> neighbours = get_unvisited_neighbours(grid, curr_x, curr_y, rows, cols);
+  shuffle(neighbours.begin(), neighbours.end(), gen);
+  
+  for (auto [nx, ny] : neighbours) {
+    if (!grid[nx][ny].visited) {
+      remove_walls(grid, curr_x, curr_y, nx, ny);
+      system("clear");
+      print_maze(grid, rows, cols);
+      std::this_thread::sleep_for(20ms);
+      dfs(grid, nx, ny, rows, cols, gen);
+    }
+  }
+}
+
 void runMazer(int rows, int cols) {
   vector<vector<node>> grid(rows, vector<node>(cols));
+  random_device rd;
+  mt19937 gen(rd());
   grid_coords_setup(grid, rows, cols);
-  dfs(grid, 0, 0, rows, cols);
-
-  print_maze(grid, rows, cols);
+  dfs(grid, 0, 0, rows, cols, gen);
 }
